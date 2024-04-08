01b1b9a793f573aeabf8d9ad9d7048a807f5559e
Om Sharma <ombhatt1158@gmail.com>
2024-04-08 14:48:02 +0530

    billboard crud added

--- app/(dashboard)/[storeId]/(routes)/billboards/components/clients.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/clients.tsx
@@ -3,24 +3,34 @@
 import { Button } from "@/components/ui/button"
 import { Heading } from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
+import { Billboard } from "@prisma/client"
 import { Plus } from "lucide-react"
 import { useParams, useRouter } from "next/navigation"
+import { BillboardColumn, columns } from "./columns"
+import { DataTable } from "@/components/ui/data-table"
 
-const clients = () => {
+
+interface BillboardClientProps{
+    data: BillboardColumn[]
+}
+
+const clients:React.FC<BillboardClientProps> = ({
+    data
+}) => {
     const router = useRouter()
     const params = useParams()
-
 
     return (
         <>
             <div className="flex items-center justify-between">
                 <Heading
-                    title="Billboards (0)" description={"Manage billboards for your store"} />
+                    title={`Billboards (${data.length})`} description={"Manage billboards for your store"} />
             <Button onClick={()=> router.push(`/${params.storeId}/billboards/new`)}>
                 <Plus className="mr-2 h-4 w-4"/>Add New
             </Button>
             </div>
             <Separator/>
+            <DataTable searchKey="label" columns={columns} data={data}/>
         </>
     )
 }
