201e02768bf7b3a7033f56a0ad9a93eb844c73c0
Om Sharma <ombhatt1158@gmail.com>
2024-04-10 21:52:10 +0530

    init storefront frontend

---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-light border-t'>
+        <div className='mx-auto py-10'>
+            <div className="text-center text-xs text-black">
+                &copy; 2-23 Myntromi 2024, Inc. All rights reserved
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
